121551e4058988263ac5954c05bda837af011b78
sofinenis <sofiacg004@gmail.com>
2025-08-28 10:14:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title( "Te amo mi negra favorita")
+st.title( "Te amo mi nalgona favorita")
 
 st.header("dame un beso")
 st.write("original en que aspecto?")
@@ -9,3 +9,6 @@ image = Image.open('gato.jpg')
 
 st.image(image, caption ='interfac multimodales')
 
+
+texto = st.text_input('Tu eres mi mundo entero mi negra', 'Ramon')
+st.write('camila llega ya plis :C', texto)
