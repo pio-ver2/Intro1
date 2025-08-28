b60fe33fb568bd795c6270a7a195dfa01c88daa4
sofinenis <sofiacg004@gmail.com>
2025-08-28 09:57:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from PIT import image
 
 st.title( "Te amo mi negra favorita")
+
+st.header("dame un beso")
+st.write("original en que aspecto?")
+image = Image.open('gato.jpg')
+
+st.image(image, caption ='interfac multimodales')
 
