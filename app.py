4836f4fe0fc8f606fd3baca7b4e3751cf73c5fa3
cvergaraa <159863822+cvergaraa@users.noreply.github.com>
2025-09-10 19:03:41 -0500

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from PIL import Image
 
-st.title( "Te amo mi nalgona favorita")
+st.title( "Oliver es un gato gordo")
 
-st.header("dame un beso")
-st.write("original en que aspecto?")
+st.header("Dile hola a Oliver")
+st.write("Este es oliver ")
 image = Image.open('Oli.jfif')
 
 st.image(image, caption ='interfac multimodales')
 
 
-texto = st.text_input('Tu eres mi mundo entero mi negra', 'Ramon')
-st.write('camila llega ya plis :C', texto)
+texto = st.text_input('Que miras', 'Holis')
+st.write('Alguien huele feo', texto)
