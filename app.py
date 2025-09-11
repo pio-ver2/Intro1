34d446918227ae84b7885c24dc57934898fe4290
cvergaraa <159863822+cvergaraa@users.noreply.github.com>
2025-09-10 19:00:14 -0500

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title( "Te amo mi nalgona favorita")
 
 st.header("dame un beso")
 st.write("original en que aspecto?")
-image = Image.open('gato.jpg')
+image = Image.open('Oli.jfif')
 
 st.image(image, caption ='interfac multimodales')
 
